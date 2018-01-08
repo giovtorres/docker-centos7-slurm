1f91a705b7a426f84218e34e93e5b77e3067ab5b
Giovanni Torres <giovanni.torres@gmail.com>
2018-01-08 11:19:49 -0500

    Updated output messages for a cleaner output.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 
-function finish {
-    echo "im done here"
-}
-
-trap finish EXIT
-
 if [ ! -f "/var/lib/mysql/ibdata1" ]; then
-    echo "Initializing database"
-    /usr/bin/mysql_install_db
-    echo "Database initialized"
+    echo "- Initializing database"
+    /usr/bin/mysql_install_db &> /dev/null
+    echo "- Database initialized"
+    echo "- Updating MySQL directory permissions"
     chown -R mysql:mysql /var/lib/mysql
     chown -R mysql:mysql /var/run/mariadb
 fi
@@ -17,37 +12,36 @@ fi
 if [ ! -d "/var/lib/mysql/slurm_acct_db" ]; then
     /usr/bin/mysqld_safe --datadir='/var/lib/mysql' &
 
-    for i in {30..0}; do
+    for count in {30..0}; do
         if echo "SELECT 1" | mysql &> /dev/null; then
             break
         fi
-        echo "Starting MariaDB"
+        echo "- Starting MariaDB to create Slurm account database"
         sleep 1
     done
 
-    if [ "$i" = 0 ]; then
+    if [[ "$count" -eq 0 ]]; then
         echo >&2 "MariaDB did not start"
         exit 1
     fi
 
-    echo "Creating Slurm acct database"
+    echo "- Creating Slurm acct database"
     mysql -NBe "CREATE DATABASE slurm_acct_db"
     mysql -NBe "CREATE USER 'slurm'@'localhost'"
     mysql -NBe "SET PASSWORD for 'slurm'@'localhost' = password('password')"
     mysql -NBe "GRANT USAGE ON *.* to 'slurm'@'localhost'"
     mysql -NBe "GRANT ALL PRIVILEGES on slurm_acct_db.* to 'slurm'@'localhost'"
     mysql -NBe "FLUSH PRIVILEGES"
-    echo "Slurm acct database created"
-    echo "Stopping MariaDB"
+    echo "- Slurm acct database created. Stopping MariaDB"
     killall mysqld
-    for i in {30..0}; do
+    for count in {30..0}; do
         if echo "SELECT 1" | mysql &> /dev/null; then
             sleep 1
         else
             break
         fi
     done
-    if [ "$i" = 0 ]; then
+    if [[ "$count" -eq 0 ]]; then
         echo >&2 "MariaDB did not stop"
         exit 1
     fi
@@ -55,7 +49,7 @@ fi
 
 chown slurm:slurm /var/spool/slurmd /var/run/slurmd /var/lib/slurmd /var/log/slurm
 
-echo "Starting all processes"
+echo "- Starting all Slurm processes under supervisord"
 /usr/bin/supervisord --configuration /etc/supervisord.conf
 
 exec "$@"
