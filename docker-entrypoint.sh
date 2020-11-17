22a341f6903df5d8b90bacad673e30ae3b3912ac
Richard Darst <rkd@zgib.net>
2020-11-17 21:19:13 +0200

    docker-entrypoint: create cluster
    - Slurm < 20.02 requires `sacctmgr add cluster linux` to be run in
      order to add the cluster to the accounting database.
    - This is a slightly long process to do this, but there are many
      errors that can occur if not all daemons are started yet, and we
      can't easily wait for all of them.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -52,4 +52,28 @@ chown slurm:slurm /var/spool/slurmd /var/run/slurmd /var/lib/slurmd /var/log/slu
 echo "- Starting all Slurm processes under supervisord"
 /usr/bin/supervisord --configuration /etc/supervisord.conf
 
+# The following is not needed in slurm > 20.02 (the cluster is created
+# automatically if it doesn't exist)
+echo "Slurm daemons are starting up..."
+for count in {30..0}; do
+    if supervisorctl status | grep -v RUNNING > /dev/null; then
+        sleep 1
+    else
+        break
+    fi
+done
+if [[ "$count" -eq 0 ]]; then
+    echo >&2 "Daemons did not start"
+    exit 1
+fi
+
+# Give a few more seconds for daemons to actually become responsive.
+echo "Slurm is initializing..."
+sleep 5
+echo "Creating cluster..."
+yes | sacctmgr add cluster linux > /dev/null
+supervisorctl restart slurmctld > /dev/null
+# Ensure slurmctld becomes responsive again
+sleep 3
+
 exec "$@"
