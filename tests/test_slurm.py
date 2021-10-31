564ad1d1660eb079b8303203956f488794a0562d
Giovanni Torres <giovanni.torres@gmail.com>
2021-10-31 00:05:55 -0400

    test: run a job and check output

--- tests/test_slurm.py
+++ tests/test_slurm.py
@@ -1,6 +1,14 @@
+import time
+
 import pytest
 
 @pytest.mark.parametrize("partition", ["normal", "debug"])
 def test_partitions_are_up(host, partition):
     partition_status = host.check_output(f"scontrol -o show partition {partition}")
     assert "State=UP" in partition_status
+
+
+def test_job_can_run(host):
+    host.run('sbatch --wrap="hostname"')
+    time.sleep(2)
+    assert host.file("slurm-1.out").content_string == "slurmctl\n"
