420fbf8be3c54cffd4761607eae21df07d5e0654
Robin Kennedy-Reid <robin.kennedy-reid@stfc.ac.uk>
2021-12-08 16:57:02 +0000

    Use variable for key directory

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -85,17 +85,18 @@ then
     error_with_msg "MariaDB did not stop"
 fi
 
+jwt_secret_dir="/etc/slurm/jwt/jwt_hs256.key"
 if [ "$JWT_SECRET" ]
 then
     echo "- JWT secret variable found, writing..."
-    echo "$JWT_SECRET" > /etc/slurm/jwt/jwt_hs256.key
+    echo "$JWT_SECRET" > $jwt_secret_dir
 else
     echo "- No JWT secret variable found, generating JWT key"
-    dd if=/dev/urandom bs=32 count=1 >/etc/slurm/jwt/jwt_hs256.key
+    dd if=/dev/urandom bs=32 count=1 >$jwt_secret_dir
 fi
 
-chown slurm:slurm /etc/slurm/jwt/jwt_hs256.key
-chmod 0600 /etc/slurm/jwt/jwt_hs256.key
+chown slurm:slurm $jwt_secret_dir
+chmod 0600 $jwt_secret_dir
 
 echo "- Starting supervisord process manager"
 /usr/bin/supervisord --configuration /etc/supervisord.conf
