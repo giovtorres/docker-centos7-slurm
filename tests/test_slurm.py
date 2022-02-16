4b153f6efff4e8436dede3314e3053bc8b144345
Per Unneberg <per.unneberg@scilifelab.se>
2022-02-16 13:38:10 +0100

    Add test for jobs on debug partition

--- tests/test_slurm.py
+++ tests/test_slurm.py
@@ -12,3 +12,15 @@ def test_job_can_run(host):
     host.run('sbatch --wrap="hostname"')
     time.sleep(2)
     assert host.file("slurm-1.out").content_string == "slurmctl\n"
+
+options = "JobID,JobName,Partition,Account,AllocCPUS,ReqMem,State,ExitCode,NodeList"
+def test_job_can_run_on_partition(host):
+    res = host.run('sinfo --long --Node')
+    print(res.stdout)
+    host.run('sbatch --wrap="hostname; sleep 5" --partition="debug"')
+    res = host.run('squeue')
+    print(res.stdout, res.stderr)
+    time.sleep(10)
+    out = host.run(f"sacct -j 1 -o {options}")
+    print(out.stdout, out.stderr)
+    assert host.file("slurm-1.out").content_string == "slurmctl\n"
