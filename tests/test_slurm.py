39354a5321e901355b4b7807514ee9fbe5a93277
Per Unneberg <per.unneberg@scilifelab.se>
2022-02-17 10:50:08 +0100

    Parametrize test_job_can_run

--- tests/test_slurm.py
+++ tests/test_slurm.py
@@ -8,14 +8,8 @@ def test_partitions_are_up(host, partition):
     assert "State=UP" in partition_status
 
 
-def test_job_can_run(host):
-    host.run('sbatch --wrap="hostname"')
-    time.sleep(2)
-    assert host.file("slurm-1.out").content_string == "slurmctl\n"
-
-
 @pytest.mark.parametrize("partition", ["normal", "debug"])
-def test_job_can_run_on_partition(host, partition):
+def test_job_can_run(host, partition):
     res = host.run(f'sbatch --parsable --wrap="hostname" --partition={partition}')
     jobid = res.stdout.strip()
     time.sleep(2)
