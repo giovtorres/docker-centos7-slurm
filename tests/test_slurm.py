a16132cdabe8608d3c587711a44178de0c12ccc2
Per Unneberg <per.unneberg@scilifelab.se>
2022-02-17 15:31:44 +0100

    Test run partition (#38)
    * Add test for jobs on debug partition
    
    * Parametrize test to run job on partition
    
    * Remove NodeHostName from all NodeName defs except c1
    
    * Parametrize test_job_can_run

--- tests/test_slurm.py
+++ tests/test_slurm.py
@@ -8,7 +8,9 @@ def test_partitions_are_up(host, partition):
     assert "State=UP" in partition_status
 
 
-def test_job_can_run(host):
-    host.run('sbatch --wrap="hostname"')
+@pytest.mark.parametrize("partition", ["normal", "debug"])
+def test_job_can_run(host, partition):
+    res = host.run(f'sbatch --parsable --wrap="hostname" --partition={partition}')
+    jobid = res.stdout.strip()
     time.sleep(2)
-    assert host.file("slurm-1.out").content_string == "slurmctl\n"
+    assert host.file(f"slurm-{jobid}.out").content_string == "slurmctl\n"
